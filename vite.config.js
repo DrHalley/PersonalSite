42b11dbcad5a04713b94413f1d7b0a20f3c4f3c8
DrHalley <75355236+DrHalley@users.noreply.github.com>
2024-05-21 21:42:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,2 @@
-import { defineConfig } from "vite";
 
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  "rewrites": [{ source: "/(.*)", destination: "/" }],
-});
+{"rewrites": [{ source: "/(.*)", destination: "/" }]}
